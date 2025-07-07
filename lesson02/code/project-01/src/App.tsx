db3edc5f901330cbd030c470d3874db17b40cb14
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-07 14:07:49 +0200

    structure was modified

--- lesson02/code/project-01/src/App.tsx
+++ lesson02/code/project-01/src/App.tsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Card from "./components/Card/Card";
 import Greeting from "./components/Greeting/Greeting";
 import ThankYou from "./components/ThankYou/ThankYou";
-import digPictures from "./assets/pudel2.jpeg";
+import digPictures from "./assets/poodle2.jpeg";
 
 function App() {
   return (
     <>
       <Greeting name={"Vasilii"} age={42} />
       <Greeting name={"Evgenii"} />
-      <Card url={"/pudel.jpeg"} alt={"Some cat"} />
+      <Card url={"/poodle.jpeg"} alt={"Some cat"} />
       <Card url={digPictures} alt={"Some cat"} />
       <ThankYou />
     </>
